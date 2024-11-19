1aad7d4759816c7206bfe55d7bbbff33c3f6ec19
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2024-11-19 09:58:58 +0530

    post

--- frontend/webeccom/src/components/admin/Admin.js
+++ frontend/webeccom/src/components/admin/Admin.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Dashboard from './Dashboard'
 
 const Admin = () => {
   return (
-    <div>admin</div>
+    <div>admin
+
+      <Dashboard/>
+    </div>
   )
 }
 
